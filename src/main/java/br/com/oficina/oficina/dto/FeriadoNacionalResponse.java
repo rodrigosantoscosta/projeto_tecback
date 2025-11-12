c4e92522349d0ea5dde37962f3e20845be6134f0
rodrigosantoscosta <rodrigosc1025@gmail.com>
2025-11-12 01:13:54 -0300

    adiciona OpenAPIConfig para o Swagger e FeriadoNacionalResponse

---
+++ src/main/java/br/com/oficina/oficina/dto/FeriadoNacionalResponse.java
@@ -0,0 +1,41 @@
+package br.com.oficina.oficina.dto;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FeriadoNacionalResponse {
+    private String date;
+    private String name;
+    private String type;
+
+    @JsonProperty("date")
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("type")
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
